8e161579645921476b7a113158588ae9abff3bd5
Agne <fizikee@gmail.com>
2019-09-19 22:25:32 -0700

    Submit and Input functionality

--- src/App.js
+++ src/App.js
@@ -10,22 +10,37 @@ import './App.css';
 const particlesOptions = {
     particles: {
     	number: {
-    		value: 270,
+    		value: 100,
     		density: {
     			enable: true,
-    			value_area: 800,
+    			value_area: 200,
     		},
     		opacity: {
-    			value: 5,
+    			value: 4,
     		}
     	},
     	size: {
-    		value: 3,
+    		value: 1,
     	}
     }
 }
 
 class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			input: '',
+		}
+	}
+
+	onInputChange = (event) => {
+		console.log(event.target.value);
+	}
+
+	onButtonSubmit = () => {
+		console.log('click');
+	}
+
 	render() {
 	  	return (
 		    <div className="App">
@@ -35,7 +50,10 @@ class App extends Component {
 		      <Navigation />
 		      <Logo />
 		      <Rank />
-			  <ImageLinkForm />
+			  <ImageLinkForm 
+			  	onInputChange={this.onInputChange}
+			  	onButtonSubmit={this.onButtonSubmit}
+			  />
 			      {/*<FaceRecognition />*/}
 		    </div>
 		);
