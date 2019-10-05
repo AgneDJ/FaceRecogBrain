cc6653e71331b4555c6984ac1c87c37e33d9f86d
Agne <fizikee@gmail.com>
2019-10-04 19:39:52 -0700

    Some corrections made to sign/reg/home

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -10,8 +10,9 @@ const Navigation = ({onRouteChange, isSignedIn}) => {
 	} else {
 		return(
 			<nav style={{display: 'flex', justifyContent: 'flex-end', }}>
-				<p onClick={()=> onRouteChange('home')} className='f3 link dim black underline pa3 pointer'>Sign In</p>
-				<p onClick={()=> onRouteChange('home')} className='f3 link dim black underline pa3 pointer'>Register</p>
+				<p onClick={()=> onRouteChange('home')} className='f3 link dim black underline pa3 pointer'>Home</p>
+				<p onClick={()=> onRouteChange('Signing')} className='f3 link dim black underline pa3 pointer'>Sign in</p>
+				<p onClick={()=> onRouteChange('Register')} className='f3 link dim black underline pa3 pointer'>Register</p>
 			</nav>
 		);
 	}
