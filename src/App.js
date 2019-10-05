498e3e46f6ea9a2ab88ea2dc93d13c9e76e18b57
Agne <fizikee@gmail.com>
2019-10-04 19:42:47 -0700

    Signin in with email, and rank count

--- src/App.js
+++ src/App.js
@@ -41,8 +41,25 @@ class App extends Component {
 			imageURL: '',
 			box: {},
 			route: 'Signing',
-			isSignedIn: false
+			isSignedIn: false,
+			user: {
+				id: '',
+				name: '',
+				email: '',
+				entries: 0, 
+				joined: ''
+			}
 		}
+	}
+
+	loadUser=(data) => {
+		this.setState({user: {
+			id: data.id,
+			name: data.name,
+			email: data.email,
+			entries: data.entries, 
+			joined: data.joined,
+		}})
 	}
 
 
@@ -69,14 +86,33 @@ class App extends Component {
 	}
 
 	onButtonSubmit = () => {
-		this.setState({imageURL: this.state.input})
+		this.setState({imageURL: this.state.input});
 		app.models
 			.predict(
 				Clarifai.FACE_DETECT_MODEL,
 				this.state.input)
-			.then(response=> this.displayFaceBox(this.calculateFaceLocation(response)))
-	    	.catch(err=>console.log(err))
+			.then(response=> {
+				if (response){
+					fetch('http://localhost:3001/image', {
+						method: 'put',
+						headers: {'Content-Type': 'application/json'},
+						body: JSON.stringify({
+							id: this.state.user.id
+						})
+					})
+					.then(response=>response.json())
+					.then(count=>{
+						this.setState(Object.assign(this.state.user, {entries: count}))
+					})
+				}
+			 
+				this.displayFaceBox(this.calculateFaceLocation(response))
+			})
+	    		.catch(err=>console.log(err));
+
 	}
+	
+
 
 	onRouteChange = (route) => {
 		if (route==='SignOut') {
@@ -98,7 +134,7 @@ class App extends Component {
 		       {route==='home'
 			  	? <div>
 				    <Logo />
-				    <Rank />
+				    <Rank name={this.state.user.name} entries={this.state.user.entries}/>
 					<ImageLinkForm 
 					  	onInputChange={this.onInputChange}
 					  	onButtonSubmit={this.onButtonSubmit}
@@ -107,8 +143,8 @@ class App extends Component {
 					</div>
 				: (
 					route==='Signing'
-					? <Signing onRouteChange={this.onRouteChange}/>
-					: <Register onRouteChange={this.onRouteChange}/>
+					? <Signing loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+					: <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
 				  )
 
 				}
