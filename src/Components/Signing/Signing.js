0cdd4f18d78b1c59ad04ce40ebfaef0fffa8361d
Agne <fizikee@gmail.com>
2019-10-04 19:42:04 -0700

    Success-> load user (signin

--- src/Components/Signing/Signing.js
+++ src/Components/Signing/Signing.js
@@ -26,11 +26,13 @@ class Signing extends React.Component {
 			})
 		})
 			.then(response=> response.json())
-			.then(data =>{
-				if(data=== 'success'){
+			.then(user =>{
+				if(user.id){
+					this.props.loadUser(user);
 					this.props.onRouteChange('home');
 				}
 			})
+
 	}
 	render(){
 		const { onRouteChange } = this.props;
