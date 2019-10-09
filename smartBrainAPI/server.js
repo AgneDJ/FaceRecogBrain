0629d1007d0d10b16fa9465754906a1a63d74085
Agne <fizikee@gmail.com>
2019-10-08 18:26:02 -0700

    Connecting name/register to db

--- smartBrainAPI/server.js
+++ smartBrainAPI/server.js
@@ -5,6 +5,22 @@ const saltRounds = 10;
 const myPlaintextPassword = 's0/\/\P4$$w0rD';
 const someOtherPlaintextPassword = 'not_bacon';
 const cors = require('cors');
+const knex= require('knex');
+
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'agnedj',
+    password : '',
+    database : 'smartbrain'
+  }
+});
+
+db.select('*').from('users').then(data=> {
+	console.log(data);
+});
 
 const app= express();
 
@@ -61,63 +77,42 @@ app.post('/signin', (req, res) => {
 });
 
 app.post('/register', (req, res)=> {
-	const{email, name, password} =req.body;
-	bcrypt.hash(password, saltRounds, function(err, hash) {
-		console.log(hash);
-	});
-
-
-	database.users.push({
-		id: '125',
-		name: name,
-		email: email,
-		entries: 0, 
-		joined: new Date()
-	});
-	res.json(database.users[database.users.length-1]);
-});
+	const{email, Name, password} =req.body;
+	db('users')
+		.returning('*')
+		.insert({
+			email: email,
+			name: Name,
+			joined: new Date()
+		})
+		.then(user=> {
+			res.json(user[0]);
+		})
+		.catch(err=>res.status(400).json('unable to register, soooorry pal'))
+})
 
 app.get('/profile/:id', (req, res) => {
 	const {id} = req.params;
-	let found = false;
-	database.users.forEach(user => {
-		if(user.id===id) {
-			found=true;
-			return res.json(user);
-		} 
-	})
-	if (!found) {
-		res.status(400).json('not found');
-	}
+	db.select('*').from('users').where({id})
+		.then(user=> {
+			if(user.length) {
+				res.json(user[0])
+			} else {
+				res.status(400).json('Not found')
+			}
+		})
+		.catch(err=>res.status(400).json())
 })
-
-
-// bcrypt.hash("bacon", null, null, function(err, hash) {
-//     // Store hash in your password DB.
-// });
-
-// // Load hash from your password DB.
-// bcrypt.compare("bacon", hash, function(err, res) {
-//     // res == true
-// });
-// bcrypt.compare("veggies", hash, function(err, res) {
-//     // res = false
-// });
-
 
 app.put('/image', (req, res)=> {
 	const {id} = req.body;
-	let found = false;
-	database.users.forEach(user => {
-		if(user.id===id) {
-			found=true;
-			user.entries++
-			return res.json(user.entries);
-		} 
+	db('users').where('id', '=', id)
+	.increment('entries', 1)
+	.returning('entries')
+	.then(entries => {
+		res.json(entries[0]);
 	})
-	if (!found) {
-		res.status(400).json('not found');
-	}
+	.catch(err=> res.status(400).json('unable to get entries'))
 })
 
 app.listen(3001, ()=> {
